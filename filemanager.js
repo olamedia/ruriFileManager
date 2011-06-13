bc48bb00fd2eb0a639ed6c352d75659ee1cff683
olamedia <olamedia@gmail.com>
2011-06-13 15:34:16 +0600

    close #1

--- filemanager.js
+++ filemanager.js
@@ -182,7 +182,35 @@ var folderExplorerPrototype = {
     copy: function(){
     console.log('TODO copy', this.list.id);
 },createFolder: function(){
-    console.log('TODO createFolder', this.list.id);
+    console.log('createFolder', this.list.id);
+    var self = this;
+    self.apiCall({
+        action: 'create', 
+        type: 'folder'
+    }, function(response){
+        if (response.status !== 0){
+            console.error(response);
+        }else{
+            var newid = response.result.id;
+            var explorer = self;
+            explorer.apiCall({
+                action: 'list', 
+                id: self.list.id
+            }, function(response){
+                console.log('response', response);
+                if (response.status !== 0){
+                    console.error(response);
+                }else{
+                    explorer.loadList(response.id, response.result);
+                    console.log('select', newid);
+                    $(explorer.list).find('a').removeClass('selected');
+                    $(explorer.list).find('li[id="' + newid + '"] a').addClass('selected');
+                    explorer.startRename();
+                }
+            });
+            console.log(response);
+        }
+    });
 },cut: function(){
     console.log('TODO cut', this.list.id);
 },deleteSelected: function(){
--- src/explorer/methods/createFolder.js
+++ src/explorer/methods/createFolder.js
@@ -1,3 +1,31 @@
 createFolder: function(){
-    console.log('TODO createFolder', this.list.id);
+    console.log('createFolder', this.list.id);
+    var self = this;
+    self.apiCall({
+        action: 'create', 
+        type: 'folder'
+    }, function(response){
+        if (response.status !== 0){
+            console.error(response);
+        }else{
+            var newid = response.result.id;
+            var explorer = self;
+            explorer.apiCall({
+                action: 'list', 
+                id: self.list.id
+            }, function(response){
+                console.log('response', response);
+                if (response.status !== 0){
+                    console.error(response);
+                }else{
+                    explorer.loadList(response.id, response.result);
+                    console.log('select', newid);
+                    $(explorer.list).find('a').removeClass('selected');
+                    $(explorer.list).find('li[id="' + newid + '"] a').addClass('selected');
+                    explorer.startRename();
+                }
+            });
+            console.log(response);
+        }
+    });
 }
